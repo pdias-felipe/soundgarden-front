cca1db4065b1595f4cbb424d6d087547a1109028
pdias-felipe <pdias.felipe@gmail.com>
2022-09-19 23:44:34 -0300

    add novos arquivos

---
+++ js/cadastro-evento.js
@@ -0,0 +1,63 @@
+
+
+function criaEvento(event){
+    
+    event.preventDefault()
+
+    const name = document.querySelector('#nome').value;
+    const attractions = document.querySelector('#atracoes').value;
+    const description = document.querySelector('#descricao').value;
+    const scheduled = document.querySelector('#data').value;
+    const number_tickets = document.querySelector('#lotacao').value;
+    
+    
+    if(!name || !name.length){
+        alert('por favor preencha o formulario corretamente antes de enviar')
+        return
+    }
+    if(!attractions){
+        alert('por favor preencha o formulario corretamente antes de enviar')
+        return
+    }
+    if(!description){
+        alert('por favor preencha o formulario corretamente antes de enviar')
+        return
+    }
+    if(!scheduled){
+        alert('por favor preencha o formulario corretamente antes de enviar')
+        return
+    }
+    if(!number_tickets || number_tickets < 1){
+        alert('por favor preencha o formulario corretamente antes de enviar')
+        return
+    }
+
+    const newEvent = {
+
+        name,
+        "attractions": attractions.split(','),
+        description,
+        scheduled,
+        number_tickets,
+
+    }
+
+    fetch("https://xp41-soundgarden-api.herokuapp.com/events",
+{
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: "POST",
+    body: JSON.stringify(newEvent)
+})
+
+.then(function(res){
+    alert('evento cadastrado com sucesso')
+    document.querySelector('#formulario').reset()
+    console.log(res)
+})
+.catch(function(res){ 
+    alert('tente novamente mais tarde')
+    console.log(res) })
+}
