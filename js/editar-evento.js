65abdda639181b8ceb8603d935c324ab5064b915
Luciana Sena <38901230+englucianasena@users.noreply.github.com>
2022-09-22 00:16:02 -0300

    Criei editar-evento.js e adicionei as informações

---
+++ js/editar-evento.js
@@ -0,0 +1,31 @@
+et body = document.getElementsByTagName("body")[0];
+body.onload = editEvent()
+const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
+
+async function pegarInfomacoes() {
+    try {
+        
+        const eventos = document.querySelector("#arows");
+        const response = await fetch(`${BASE_URL}/events`, { method: "GET" });
+        
+        const infoEvento = await response.json()
+        mostrarInformacoes(infoEvento)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function mostrarInformacoes(data) {
+    let dataEvento = data.scheduled.toLocaleString()
+
+    document.getElementById('nome').value = data.name
+    document.getElementById('banner').value = data.poster
+    document.getElementById('atracoes').value = data.attractions
+    document.getElementById('descricao').value = data.description
+    document.getElementById('data').value = dataEvento.slice(0, -3)
+    document.getElementById('lotacao').value = data.number_tickets
+
+    console.log('Data: ' + dataEvento.slice(0, -3))
+}
+
+pegarInfomacoes()
