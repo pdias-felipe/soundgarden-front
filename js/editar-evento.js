9374608b0cfa979d40cc1bf9a2da686170b34f04
Luciana Sena <38901230+englucianasena@users.noreply.github.com>
2022-09-22 14:59:24 -0300

    Update editar-evento.js

--- js/editar-evento.js
+++ js/editar-evento.js
@@ -1,31 +1,51 @@
-let body = document.getElementsByTagName("body")[0];
-body.onload = editEvent()
+const elemento = document.querySelector("body");
+const tabela = document.querySelector("tabelaEventos");
+
+// Link API
 const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
 
-async function pegarInfomacoes() {
+elemento.onload = async () => {
     try {
+        // Constantes API
+        const linkEventos = await fetch(`${BASE_URL}/events`, { method: "GET" });
+        const linkEventosJson = await linkEventos.json();
         
-        const eventos = document.querySelector("#arows");
-        const response = await fetch(`${BASE_URL}/events`, { method: "GET" });
+        // Laço
+        for (let index = 0; index < 10; index++) {
+            // dd/mm/yyyy
+            const dataEvento = new Date(linkEventosJson[index].scheduled);
         
-        const infoEvento = await response.json()
-        mostrarInformacoes(infoEvento)
-    } catch (error) {
-        console.log(error)
+            tabela.insertAdjacentHTML{
+                ."beforend",
+                <tr>
+                <th scope="row">
+                    ${index + 1}
+                </th>
+                <td>
+                    ${dataEvento.toLocalDateString("pt-BR")}
+                </td>
+                <td>
+                    ${linkEventosJson[index].name}}
+                </td>
+                <td>
+                    <a href="reservas.html?id=${
+                        linkEventosJson[index]._id
+                    }"class="btn btn-dark">
+                    ver reservas</a>
+                    <a href="editar-evento.html?id=${
+                        linkEventosJson[index]._id
+                    }"class="btn btn-secondary">editar</a>
+                    <a href="excluir-evento.html?id=${
+                        linkEventosJson[index]._id
+                    }"class="btn btn-danger">excluir</a>      
+                </td>
+              </tr>
+            };
+                
+        }
+       }
+  } catch (error) {
+    console.log(error)
+    alert("Ocorreu um erro de " + error);
     }
-}
-
-function mostrarInformacoes(data) {
-    let dataEvento = data.scheduled.toLocaleString()
-
-    document.getElementById('nome').value = data.name
-    document.getElementById('banner').value = data.poster
-    document.getElementById('atracoes').value = data.attractions
-    document.getElementById('descricao').value = data.description
-    document.getElementById('data').value = dataEvento.slice(0, -3)
-    document.getElementById('lotacao').value = data.number_tickets
-
-    console.log('Data: ' + dataEvento.slice(0, -3))
-}
-
-pegarInfomacoes()
+};
