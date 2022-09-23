18072aba2099c95ee932a64c8b88ba2e941c3132
Luciana Sena <38901230+englucianasena@users.noreply.github.com>
2022-09-23 13:49:40 -0300

    Ajustes no reserva.js

--- js/reservas.js
+++ js/reservas.js
@@ -1,33 +1,31 @@
 async function getReservas() {
-    try {
-      const divReservas = document.querySelector("#reservas");
-      const response = await fetch("https://xp41-soundgarden-api.herokuapp.com/bookings", {
-        headers: {
-          Authorization: "token",
-        },
-      });
-      const listReservas = await response.json();
+  const queryString = window.location.search
+  const urlParams = new URLSearchParams(queryString)
+  const id = urlParams.get('id')
+
+  const URL = 'https://xp41-soundgarden-api.herokuapp.com/bookings/event/' + id
   
-      listReservas.forEach((reservas) => {
+  try {
+      const divReservas = document.querySelector("#reservas");
+      const response = await fetch("https://xp41-soundgarden-api.herokuapp.com/bookings")
+      const listReservas = await response.json()
+  
+      listReservas.forEach((reservas, index) => {
         const html = `
-            <div>
-                <label>id: </label> <input type="text" value="${reservas._id}" readonly> <br>
-                <label>nome: </label> <input type="text" value="${reservas.owner_name}" readonly> <br>
-                <label>email: </label> <input type="text" value="${reservas.owner_email}" readonly> <br>
-                <label>Num. tickets: </label> <input type="text" value="${reservas.number_tickets}" readonly> <br>
-                <label>evento: </label> <input type="text" value="${reservas.event.name}" readonly> <br>
-                <label>data: </label> <input type="text" value="${reservas.created_at}" readonly> <br>
-            </div> 
+        <th scope="row">${index + 1}</th>
+        <td>${reservas.owner_name}</td>
+        <td>${reservas.owner_email}</td>
+        <td>${reservas.number_tickets}</td>
             `;
   
         divReservas.innerHTML += html;
-      });
+      })
   
-      console.log(listReservas);
+      console.log(listReservas)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
   
-  getReservas();
+  getReservas()
   
